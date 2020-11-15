f2e78587bcb70a7a086817fbd5ba9fcaa664f610
Michael <mccannm@clarkson.edu>
2020-11-14 19:20:29 -0500

    test

---
+++ src/sample/Main.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FlowPane root= new FlowPane();
+        Scene scene =new Scene(root, 300, 275);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
